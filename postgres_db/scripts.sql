d85ab41fd6786ea6873d158c38376e99c370cdd2
echaparroa-uniandes <e.chaparroa@uniandes.edu.co>
2024-04-10 12:25:04 -0500

    actualizacion de estados en la BD - inicial

--- postgres_db/scripts.sql
+++ postgres_db/scripts.sql
@@ -32,7 +32,13 @@ CREATE TABLE IF NOT EXISTS split_videos (
     id SERIAL PRIMARY KEY,
     id_video INTEGER REFERENCES videos(id),  -- Cambiado el tipo de dato a INTEGER
     split_path VARCHAR(255),
-    _order INT,
+    split_order INT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+--------------------------------------------------------------
+--Usuario de prueba
+INSERT INTO public.usuarios
+(id, username, email, pass, created_at, updated_at)
+VALUES(1, 'usuario1', 'usuario1@gmail.com', '12345', '2024-04-10', '2024-04-10');
