732f2f567c62aec2b208f08b5ab7fca1d3ba9d32
jcamilovallejos <j.vallejosg@uniandes.edu.co>
2024-04-10 21:53:55 -0500

    Adicionar procesamiento de videos

---
+++ video-processing/app.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+import os
+from moviepy.editor import VideoFileClip, concatenate_videoclips, ImageClip
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'videos/uploads'
+UPLOAD_FOLDER_TO_PROCESSED_VIDEOS = 'videos/processed'
+ALLOWED_EXTENSIONS = {'mp4'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER_TO_PROCESSED_VIDEOS'] = UPLOAD_FOLDER_TO_PROCESSED_VIDEOS
+
+# Path to the logo image
+LOGO_PATH = 'logo.jpg'
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def shorten_video_duration(video_clip):
+    if video_clip.duration > 20:
+        return video_clip.subclip(0, 20)
+    return video_clip
+
+def add_logo_to_video(video_clip):
+    logo_clip = ImageClip(LOGO_PATH, duration=1)
+    return concatenate_videoclips([logo_clip, video_clip, logo_clip])
+
+def resize_video_to_16_9(video_clip):
+    width, _ = video_clip.size
+    height = int(width * 9 / 16)
+    return video_clip.resize(height=height).crop(x_center=width / 2, y_center=height / 2, width=width, height=height)
+
+def save_processed_video(video_clip, filename):
+    processed_filename = filename
+    processed_video_path = os.path.join(app.config['UPLOAD_FOLDER_TO_PROCESSED_VIDEOS'], processed_filename)
+    video_clip.write_videofile(processed_video_path, codec='libx264', fps=24)
+    return processed_video_path
+
+#Use this function to processed video that come from files container
+def process_video(file):
+    filename = secure_filename(file.filename)
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(file_path)
+
+    original_video = VideoFileClip(file_path)
+    processed_video = shorten_video_duration(original_video)
+    processed_video = add_logo_to_video(processed_video)
+    processed_video = resize_video_to_16_9(processed_video)
+
+    return save_processed_video(processed_video, filename)
+
+@app.route('/upload-form', methods=['GET', 'POST'])
+#Only for testing purposes
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            processed_video_path = process_video(file)
+            return render_template('success.html', filename=os.path.basename(processed_video_path))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+
+@app.route('/uploads/<filename>')
+#Only for testing purposes
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5003)
