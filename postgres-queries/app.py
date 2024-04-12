f8ae8f3c652f2547e3ff507b012d36bfa37866b6
jcamilovallejos <j.vallejosg@uniandes.edu.co>
2024-04-12 17:19:27 -0500

    Adicionar ajustes de tablas y de postgres

--- postgres-queries/app.py
+++ postgres-queries/app.py
@@ -1,5 +1,6 @@
 import os
 import psycopg2
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # Obtener las variables de entorno
 DB_NAME = os.getenv("POSTGRES_DB")
@@ -18,12 +19,14 @@ connection = psycopg2.connect(
 # Crear un cursor para ejecutar comandos SQL
 cursor = connection.cursor()
 
+password = generate_password_hash('12345')
+
 # Definir sentencias SQL para crear tablas
 create_tables_query = """
 CREATE TABLE IF NOT EXISTS usuarios (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(255),
-    email VARCHAR(255),
+    username VARCHAR(255) UNIQUE,
+    email VARCHAR(255) UNIQUE,
     pass VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -62,13 +65,14 @@ CREATE TABLE IF NOT EXISTS split_videos (
 );
 --------------------------------------------------------------
 --Usuario de prueba
+
 INSERT INTO public.usuarios
 (username, email, pass, created_at, updated_at)
-VALUES('usuario1', 'usuario1@gmail.com', '12345', '2024-04-10', '2024-04-10');
+VALUES('usuario1', 'admin@gmail.com', %s, '2024-04-10', '2024-04-10');
 """
 
 # Ejecutar las sentencias SQL
-cursor.execute(create_tables_query)
+cursor.execute(create_tables_query, (password,))
 
 # Confirmar los cambios
 connection.commit()
